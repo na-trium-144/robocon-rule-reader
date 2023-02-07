a1d48f3d027be0dd8339b3a0158c5ae136d3be29
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2023-02-07 18:17:46 +0900

    appbar,list

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+// import '@/styles/globals.css'
+import { Inter } from "@next/font/google";
+import AppBar from "components/appbar";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter({ subsets: ["latin"] });
+
+export default function App({ Component, pageProps }) {
+  return (
+    <div className={inter.className}>
+      <AppBar />
+      <Component {...pageProps} />
+    </div>
+  );
 }
